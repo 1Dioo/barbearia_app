c2ea515f00ab0cad2cbe09ace58e81057a9601d0
Diogo <diogorg6@gmail.com>
2026-06-13 17:08:38 -0300

    Documentação main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+/*
+* Arquivo principal da aplicação Royal Barber.
+* Responsável por inicializar o Flutter, configurar
+* a localização em português (pt-BR) e executar
+* o widget principal da aplicação (BarberApp).
+*/
+
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'app.dart';
