9d6806b53698a9639b0d3c2581e23a43437f0ac9
Diogo <diogorg6@gmail.com>
2026-06-13 17:45:11 -0300

    Update app_brand.dart

--- lib/core/app_brand.dart
+++ lib/core/app_brand.dart
@@ -1,3 +1,11 @@
+/*
+  Classe responsável por centralizar
+  as informações da marca do aplicativo.
+
+  Armazena o nome da barbearia e
+  o slogan utilizado nas telas.
+*/
+
 class AppBrand {
   static const appName = 'Royal Barber';
   static const slogan = 'Seu estilo, nossa arte';
